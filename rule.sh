86d17e69f771722771585892ac408010d35a4945
kevlau1 <87843207+kevlau1@users.noreply.github.com>
2022-01-09 00:13:51 +0800

    Update rule.sh

--- rule.sh
+++ rule.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd /etc/XrayR
 wget https://raw.githubusercontent.com/kevlau1/rulelist/main/rulelist
 sudo sed -i 's/RuleListPath:/RuleListPath: \/etc\/XrayR\/rulelist/g' /etc/XrayR/config.yml
